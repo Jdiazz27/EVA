17b96ab904441ff24dea2782a2181fcab4ce8a21
jdiaz <jdiaz@10.20.3.45>
2024-11-05 11:36:08 -0500

    Commit inicial

---
+++ src/eva/Eva.java
@@ -0,0 +1,20 @@
+package eva;
+
+
+/**
+ *
+ * @author alesa
+ */
+public class Eva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        F1 frame = new F1();
+        frame.setVisible(true);
+        frame.setSize(450, 610);        
+        
+
+    }
+}
